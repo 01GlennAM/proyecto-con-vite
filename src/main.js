9c7c27ffd2261fc71fc5aa00a48a1cf6f5d8295b
01GlennAM <shunretrofied@gmail.com>
2026-01-07 17:40:07 -0500

    se crea proyecto grande con vite

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './css/normalize.css';
+import './css/variables.css';
+import './css/estilos.css'; /* se improta acá y no en el index para que pase por vite y sea optimizado */ 
+import './css/header.css';
