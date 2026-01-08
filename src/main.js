4b8e5fd220c767bf17ae2edfd00304d1d8ba489e
01GlennAM <shunretrofied@gmail.com>
2026-01-08 15:57:00 -0500

    se crea la segunda seccion de imagenes y de thump

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import './css/normalize.css';
 import './css/variables.css';
 import './css/estilos.css'; /* se improta acá y no en el index para que pase por vite y sea optimizado */ 
 import './css/header.css';
+import './css/hero.css';
+import './css/caracteristicas.css';
